5267cb954dc090d502c5ce4b0ec989dceb0bba78
t343s <t343s@PC-01>
2025-11-05 19:58:19 +0300

    add dto

---
+++ WebAPI/DTO/CandidateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.DTO;
+
+public class CandidateDto
+{
+    [Required]
+    public int? Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Surname { get; set; }
+    [Required]
+    public string? Program { get; set; }
+}
